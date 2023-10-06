ccb45d03e90e88fc25d7a67c89187fb74ec5c110
SuperSonicYT <84252471+SuperSonicYT@users.noreply.github.com>
2023-10-06 20:01:39 +0530

    commit-backend

---
+++ Backend-Development/index.php
@@ -0,0 +1,10 @@
+<?php $username = 'Akash' ?>
+
+<html>
+    <head>
+        <title>PHP Tutorial</title>
+    </head>
+    <body>
+        <h3>Hello <?php echo $username ?></h3>
+    </body>
+</html>
